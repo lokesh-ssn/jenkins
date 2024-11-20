b0af64a333e015f888b955391f8bf1b9d832c949
lokesh-ssn <lokesh2110483@ssn.edu.in>
2024-11-20 15:14:50 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -10,14 +10,14 @@ public class hello {
     
         // ask users to enter operator
         System.out.println("Choose an operator: +, -, *, or /");
-        operator = input.next().charAt(0);
+        operator = '+';//input.next().charAt(0);
     
         // ask users to enter numbers
-        System.out.println("Enter first number");
-        number1 = input.nextDouble();
+        System.out.println("Enter first number: 5");
+        number1 = nextDouble(5);//input.nextDouble();
     
-        System.out.println("Enter second number");
-        number2 = input.nextDouble();
+        System.out.println("Enter second number: 5");
+        number2 = nextDouble(5); //input.nextDouble();
     
         switch (operator) {
     
